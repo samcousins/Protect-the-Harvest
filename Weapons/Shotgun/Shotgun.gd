bcfec5ef3b61ec6880399fb9dd8783a3ce15d404
samcousins <samuel.lunncousins@gmail.com>
2023-01-06 22:51:36 +0000

    shotgun spread

--- Weapons/Shotgun/Shotgun.gd
+++ Weapons/Shotgun/Shotgun.gd
@@ -2,13 +2,16 @@ extends MeshInstance
 
 var bullet_sc = preload("res://Weapons/Bullet.tscn")
 
+export var number_of_shells := 5
+
 func shoot():
-	var bullet = bullet_sc.instance()
-	
-	bullet.global_translation = $Barrel.global_translation
-	
-	bullet.direction = get_global_transform().basis.z
-	bullet.direction.x += rand_range(-1.0, 1.0)
-	bullet.direction.y += rand_range(-1.0, 1.0)
-	
-	get_tree().root.add_child(bullet)
+	for n in number_of_shells:
+		var bullet = bullet_sc.instance()
+		
+		bullet.global_translation = $Barrel.global_translation
+		
+		bullet.direction = get_global_transform().basis.z
+		bullet.direction.x += rand_range(-2.0, 2.0)
+		bullet.direction.y += rand_range(-2.0, 2.0)
+		
+		get_tree().root.add_child(bullet)
