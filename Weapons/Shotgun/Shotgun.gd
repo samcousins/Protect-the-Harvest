fdab164382d3424e0c54449e4eaa5814b17cc5a2
samcousins <samuel.lunncousins@gmail.com>
2023-01-06 23:10:18 +0000

    Update Shotgun.gd

--- Weapons/Shotgun/Shotgun.gd
+++ Weapons/Shotgun/Shotgun.gd
@@ -2,7 +2,8 @@ extends MeshInstance
 
 var bullet_sc = preload("res://Weapons/Bullet.tscn")
 
-export var number_of_shells := 5
+export var number_of_shells := 10
+var spread := 1.5
 
 func shoot():
 	for n in number_of_shells:
@@ -11,7 +12,8 @@ func shoot():
 		bullet.global_translation = $Barrel.global_translation
 		
 		bullet.direction = get_global_transform().basis.z
-		bullet.direction.x += rand_range(-2.0, 2.0)
-		bullet.direction.y += rand_range(-2.0, 2.0)
+		bullet.direction.z += rand_range(-spread, spread)
+		bullet.direction.y += rand_range(-spread, spread)
+		bullet.direction.x += rand_range(-spread, spread)
 		
 		get_tree().root.add_child(bullet)
