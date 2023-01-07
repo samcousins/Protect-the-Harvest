09a53e3e4b1f9c617361ae3b41f157edcbc75e38
samcousins <samuel.lunncousins@gmail.com>
2023-01-07 09:39:25 +0000

    badger kills

--- Enemies/Badger/Badger.gd
+++ Enemies/Badger/Badger.gd
@@ -12,19 +12,10 @@ var gravity = -60
 
 var velocity := Vector3()
 
+var can_attack := true
+
 func _ready():
-	
-	var targets := get_tree().get_nodes_in_group("objectives")
-	
-	var distance := INF
-	
-	for target in targets:
-		var new_dist = global_translation.distance_squared_to(target.global_translation)
-		if new_dist < distance:
-			distance = new_dist
-			current_target = target
-	
-	agent.set_target_location(current_target.global_translation)
+	find_new_target()
 
 func take_damage(dmg):
 	print("Took damage")
@@ -33,7 +24,15 @@ func take_damage(dmg):
 		die()
 
 func _physics_process(delta):
+	if not current_target or not is_instance_valid(current_target):
+		find_new_target()
+		return
+	
+	look_at(Vector3(current_target.global_translation.x, global_translation.y, current_target.global_translation.z), Vector3.UP)
+	
 	if agent.distance_to_target() < agent.target_desired_distance:
+		if can_attack:
+			attack()
 		return
 	
 	var dest = agent.get_next_location()
@@ -46,3 +45,28 @@ func _physics_process(delta):
 
 func die():
 	queue_free()
+
+func attack():
+	current_target.attacked(1)
+	can_attack = false
+	$Cooldown.start()
+
+func _on_Cooldown_timeout():
+	can_attack = true
+
+func find_new_target():
+	var targets := get_tree().get_nodes_in_group("objectives")
+	
+	if targets.size() == 0 or not targets:
+		return
+	
+	var distance := INF
+	
+	for target in targets:
+		var new_dist = global_translation.distance_squared_to(target.global_translation)
+		if new_dist < distance:
+			distance = new_dist
+			current_target = target
+	
+	if current_target:
+		agent.set_target_location(current_target.global_translation)
--- Objectives/Hay/Hay.gd
+++ Objectives/Hay/Hay.gd
@@ -1,5 +1,14 @@
 extends StaticBody
 
+var hp := 2
 
 func _ready():
 	add_to_group("objectives")
+
+func attacked(dmg):
+	hp -= dmg
+	if hp <= 0:
+		die()
+
+func die():
+	queue_free()
