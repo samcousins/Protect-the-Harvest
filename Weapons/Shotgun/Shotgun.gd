d9eb972f7e36feb7de3cdf1c10b07507b3cd7b4d
samcousins <samuel.lunncousins@gmail.com>
2023-01-08 11:58:32 +0000

    working on power up logic

--- Weapons/Shotgun/Shotgun.gd
+++ Weapons/Shotgun/Shotgun.gd
@@ -6,12 +6,13 @@ export var number_of_shells := 20
 var spread := 1.0
 
 var can_shoot := true
+var powered_up := false
 
 onready var cooldown : Timer = $Cooldown
 var cooldown_time := 0.5
 onready var anim : AnimationPlayer = $AnimationPlayer
 
-onready var power_up_timer : Timer = $PowerUpTimer
+onready var power_up_timer : Timer = $PowerUp
 
 func _ready():
 	cooldown.wait_time = cooldown_time
@@ -36,10 +37,13 @@ func _on_Cooldown_timeout():
 	can_shoot = true
 
 func power_up(power_time):
-	anim.playback_speed = 2
-	cooldown.wait_time = cooldown_time/2
-	power_up_timer.wait_time  = power_time
-	power_up_timer.start()
+	if not powered_up:
+		anim.playback_speed = 2
+		cooldown.wait_time = cooldown_time/2
+		power_up_timer.wait_time = power_time
+		power_up_timer.start()
+	else:
+		power_up_timer.remaining_time = power_time
 
 func _on_PowerUp_timeout():
 	power_down()
@@ -47,5 +51,6 @@ func _on_PowerUp_timeout():
 func power_down():
 	anim.playback_speed = 1
 	cooldown.wait_time = cooldown_time
+	powered_up = false
 
 
