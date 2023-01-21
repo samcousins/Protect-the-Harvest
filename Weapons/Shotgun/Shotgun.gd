04490249cdc22cedca6b07cb2e07028295142fa5
samcousins <samuel.lunncousins@gmail.com>
2023-01-21 18:29:00 +0000

    replacing old shotgun

--- Weapons/Shotgun/Shotgun.gd
+++ Weapons/Shotgun/Shotgun.gd
@@ -6,31 +6,29 @@ var bullets_in_shell := 20
 var shell_spread := .3
 var shell_count := 0
 
-var can_shoot := true
-
 var sped_up := false
 
-onready var cooldown : Timer = $Cooldown
-var cooldown_time := 0.5
 onready var anim : AnimationPlayer = $AnimationPlayer
 
+onready var shoot_sound := $Shoot
+
 onready var speed_up_timer : Timer = $SpeedUpTimer
-onready var speed_up_anim = $ShotgunCroc/SpeedUpAnim
-onready var shells_count_ui = $ShotgunCroc/ShellsCount
+
+onready var speed_up_anim = $Node2/root/body/SpeedUpAnim
+
+onready var shells_count_ui = $Node2/root/body/ShellsCount
+
+onready var barrel := $Node2/root/barrel/bottom_barrel_hole
 
 
 func _ready():
 	shells_count_ui.visible = false
 	speed_up_anim.visible = false
-	cooldown.wait_time = cooldown_time
 
 
 func shoot():
-	if not can_shoot:
+	if anim.is_playing():
 		return
-	
-	can_shoot = false
-	cooldown.start()
 	
 	if shell_count > 0:
 		use_shell()
@@ -40,22 +38,18 @@ func shoot():
 
 func use_bullet():
 	spawn_projectile(0)
-	anim.play("Cooldown")
-	$Shoot.play()
+	anim.play("shoot_normal")
+	shoot_sound.play()
 
 
 func spawn_projectile(spread):
 	randomize()
 	var bullet = bullet_sc.instance()
-	bullet.transform = $Barrel.global_transform
+	bullet.transform = barrel.global_transform
 	bullet.rotation.x += rand_range(-(spread*0.25), spread*0.25)
 	bullet.rotation.y += rand_range(-spread, spread)
 	bullet.velocity = bullet.transform.basis.z * bullet.muzzle_velocity
 	get_tree().root.add_child(bullet)
-
-
-func _on_Cooldown_timeout():
-	can_shoot = true
 
 
 func power_up(power_name, power_time):
@@ -67,11 +61,12 @@ func power_up(power_name, power_time):
 
 func speed_up(power_time):
 	speed_up_anim.visible = true
+	
 	if not sped_up:
 		anim.playback_speed = 2
-		cooldown.wait_time = cooldown_time/2
 		speed_up_timer.wait_time = power_time
 		speed_up_timer.start()
+	
 	else:
 		speed_up_timer.remaining_time = power_time
 
@@ -86,8 +81,10 @@ func load_shells(power_time):
 func use_shell():
 	for n in bullets_in_shell:
 		spawn_projectile(shell_spread)
-	anim.play("ShotgunCooldown")
-	$Shoot.play()
+		
+	anim.play("shoot_shells")
+	shoot_sound.play()
+	
 	shell_count -= 1
 	shells_count_ui.text = str(shell_count)
 	if shell_count == 0:
@@ -97,7 +94,6 @@ func use_shell():
 func speed_power_down():
 	speed_up_anim.visible = false
 	anim.playback_speed = 1
-	cooldown.wait_time = cooldown_time
 	sped_up = false
 
 
